0080a57f597ac2d44eee0246d7717e90df2e1cd1
moses-lal <moseslal2692@gmail.com>
2025-08-07 16:03:38 +0530

    CloudStoreApp completed

--- JokesApp/script.js
+++ JokesApp/script.js
@@ -1,1 +1,14 @@
-function 
+async function getJokes(){
+    const response = await fetch("https://v2.jokeapi.dev/joke/Any?type=twopart") ;
+
+    const data = await response.json();
+
+    const joke = document.getElementById("welcome");
+
+    const punchline = document.getElementById("punchline");
+
+    joke.innerText=data.setup;
+
+    punchline.innerText=data.delivery;
+
+}
--- MakeUpapp/Script/product.js
+++ MakeUpapp/Script/product.js
@@ -28,14 +28,16 @@ async function getItem() {
               element.api_featured_image
             }" alt="" class=" my-3 mx-4 d-block shadow img" width="200px" height="200px"   />
             <h6 class="my-3">
+            <a href="../pages/productdetails" onclick="description()" class=" product text-decoration-none ">
              ${element.name}
             </h6>
+            </a>
           </div>
 
           <div class="d-flex flex-column gap-1">
             <span class="fs-6 text-success">MRP: ₹ ${(element.price * 85).toFixed()}</span>
             <span class="fs-6 text-info">brand : ${element.brand}</span>
-            <span class="fs-6 text-info">Category : ${ (element.category== "") ? (element.category=="NA") : element.category }</span>
+            <span class="fs-6 text-info">Category : ${ (element.category== "") ? "NA" : element.category }</span>
             <span class="fs-6 text-info"
               >Rating
               <span
@@ -50,13 +52,13 @@ async function getItem() {
                   <path
                     d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"
                   /></svg></span
-              > : 4.5/5 (41 revies)</span
+              > : ${(element.rating===null)? "no rating" : element.rating}/5 (41 revies)</span
             >
-            <span class="fs-6 text-white"> product_type : ${
+             <span class="fs-6 text-white"> product_type : ${
               element.product_type
-            } </span>
+             } </span>
           </div>
-        
+        <div>
         `;
 
     productList.appendChild(d);
@@ -64,3 +66,10 @@ async function getItem() {
 }
 
 getItem();
+
+
+
+
+function description(){
+
+}
