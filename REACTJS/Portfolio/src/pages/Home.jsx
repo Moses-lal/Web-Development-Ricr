b19dd51127e395b85dd090796b8293219ecbd922
moses-lal <moseslal2692@gmail.com>
2025-08-22 07:29:06 +0530

    Portfolio using React

--- REACTJS/Portfolio/src/pages/Home.jsx
+++ REACTJS/Portfolio/src/pages/Home.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React from "react";
+import moses from "../assets/moses.jpg";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <div>
+        <div className="w-full h-screen bg-[url('/hero.jpg')] bg-contain bg-start  ">
+         <div className="w-179 h-180 bg-[#5c200c] mx-110 p-5 " > 
+          <div className="flex gap-20">
+            <h2 className="text-4xl font-bold text-amber-50 mt-8 ms-5"> Hello, <br />I m <span className="text-blue-900 font-extrabold">Moses Lal</span>  </h2>
+            <div className="w-70 h-70 border rounded-full mt-1 ">
+              <img src={moses} className="object-cover w-70 h-70 rounded-full object-top shadow-2xl shadow-amber-200 border-4 border-b-cyan-800 border-s-cyan-800 border-e-red-900 border-t-red-900" />
+            </div>
+          </div>
+            <h3 className="text-xl text-white">I' M a Full Stack Web Developer</h3>
+            
+         </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-
-export default Home; 
+export default Home;
--- REACTJS/Portfolio/src/pages/Project.jsx
+++ REACTJS/Portfolio/src/pages/Project.jsx
@@ -1,8 +1,56 @@
 import React from 'react'
+import img1 from '../assets/virtual-market-place.png'
+import img2 from '../assets/img2.png'
 
  const Project = () => {
   return (
-    <div>Project</div>
+    <>
+    
+     <section>
+      <div className=' p-2  bg-green-300'>
+         <h2 className='text-3xl text-center me-52 mt-8 '> Featured Projects</h2>
+      </div>
+
+      <div className='flex bg-blue-950'>
+        <div className='ms-8 mt-10 border p-5 rounded-3xl bg-emerald-100 '>
+          <img src={img1} width="400px" height="400px" className='rounded-2xl shadow-md shadow-blue-700'/>
+          <h3 className='text-2xl mt-3 font-bold text-blue-800'>Virtual Market place -  </h3>
+          <p className='font-bold text-blue-400'>a market for fresher to start earning and gain experience</p>
+          <p className='mt-3 text-orange-700'>Made In - Full Stack using MERN</p>
+          <p className='text-lime-600'>Status - Working and not live </p>
+          <p className='text-blue-900'>More Info - <ul>
+            <li>- a platform for freelancers and clients to collaborate efficiently</li>
+            <li>- Integrated secure payment gateway, escrow system,  <br />real-time chat, and feedback system</li>
+            <li>- Used MERN stack for scalable performance and security</li>
+            <li>- Scalable and much in demand </li>
+            </ul>
+            
+            </p>
+
+        </div>
+
+         <div className='ms-8 mt-10 border p-5 rounded-3xl bg-emerald-100'>
+          <img src={img2} width="400px" height="400px" className='rounded-2xl shadow-md shadow-blue-700'/>
+          <h3 className='text-2xl mt-3 font-bold text-blue-800'>E- Learning Platform - </h3>
+          <p className='font-bold text-blue-400'>a market for fresher to start earning and gain experience</p>
+          <p className='mt-3 text-orange-700'>Made In - Full Stack using MERN</p>
+          <p className='text-lime-600'>Status - Working and not live </p>
+          <p className='text-blue-900'>More Info - <ul>
+            <li>- a platform for freelancers and clients to collaborate efficiently</li>
+            <li>- Integrated secure payment gateway, escrow system,  <br />real-time chat, and feedback system</li>
+            <li>- Used MERN stack for scalable performance and security</li>
+            <li>- Scalable and much in demand </li>
+            </ul>
+            
+            </p>
+
+        </div>
+      </div>
+     
+      
+     </section>
+    </>
+   
   )
 }
 
