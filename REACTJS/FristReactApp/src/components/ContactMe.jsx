9af5c0164a66227f7b095360de4c8c22d4e62728
moses-lal <moseslal2692@gmail.com>
2025-09-15 22:14:32 +0530

    contact page completed

--- REACTJS/FristReactApp/src/components/ContactMe.jsx
+++ REACTJS/FristReactApp/src/components/ContactMe.jsx
@@ -1,14 +1,68 @@
 import React from "react";
+import { CiMail } from "react-icons/ci";
+import { FaPhone } from "react-icons/fa6";
+import { FaLocationDot } from "react-icons/fa6";
 
 
 function ContactMe(){
 
     return(
       <>
-      <div id="contact">
-        <h3 className="bg-primary-subtle p-3">
+      <div className="p-5 maincontact" id="contact">
+
+
+
+
+        <h2 className=" p-4 fw-bolder text-white text-center">
        Contact Me 
-      </h3>
+      </h2>
+        
+        <div className="d-flex container-fluid align-content-center justify-content-center bg-gradient w-75 py-2" id="cont">
+          <div className="border mt-4 w-75 text-light p-4 rounded-4 d-grid" id="cont1">
+          <div className="border p-4 d-grid gap-5 rounded-5 " id="cont2">
+            <h3 className="fw-bolder" id="cont3">Contact <br />Information</h3>
+
+            <div>
+              <div className="mb-4 "><CiMail  className="fs-2 cont4"/> <span className="fs-5 fw-medium mx-2">moseslal2692@gmail.com</span> </div>
+            <div className="mb-4"><FaPhone className="fs-3 cont4"/> <span className="fs-5 fw-medium mx-2">+91 8269672164</span></div>
+            <div className="mb-4"><FaLocationDot  className="fs-3 cont4"/> <span className="fs-5 fw-medium mx-2">Bhopal , Madhya Pradesh</span></div>
+            </div>
+              
+
+            <p className="fs-5 fw-medium">I usually reply within 24 hours, <br />
+             For urgent queries, please use email.</p>
+          </div>
+
+          <div className=" p-4 d-grid gap-3">
+            <h3 className="fw-bold cont4" >Send Message</h3>
+            <div>
+               <input type="text" placeholder="your name" className="bg-dark text-white rounded-2 p-2"/>
+              <input type="text" placeholder="your email" className="bg-dark text-white rounded-2 p-2 mx-4"/>
+            </div>
+
+             <div>
+               <input type="text" placeholder="Phone" className="bg-dark text-white rounded-2 p-2"/>
+              <input type="text" placeholder="Company" className="bg-dark text-white rounded-2 p-2 mx-4"/>
+            </div>
+
+             <div>
+               <input type="text" placeholder="Website" className="bg-dark text-white rounded-2 p-2"/>
+              <input type="text" placeholder="Reason" className="bg-dark text-white rounded-2 p-2 mx-4"/>
+            </div>
+
+            <textarea cols={50} rows={6} className="bg-dark text-white rounded-2 p-2 mx-2" placeholder="Message"></textarea>
+            
+            <div className="mt-2">
+            <button className="bg-dark text-white rounded-2 p-2 mx-5 w-75 ">Send Message</button>
+
+            </div>
+            
+          </div>
+        </div>
+        </div>
+        
+
+
       </div>
      
       </>
