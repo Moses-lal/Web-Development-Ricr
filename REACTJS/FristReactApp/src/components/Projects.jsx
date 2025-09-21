11963db2d8f7b8cecdf4c04f26fff59746b727ca
moses-lal <moseslal2692@gmail.com>
2025-09-22 00:32:40 +0530

    portfolio completed using bootstrap

--- REACTJS/FristReactApp/src/components/Projects.jsx
+++ REACTJS/FristReactApp/src/components/Projects.jsx
@@ -7,27 +7,27 @@ function Projects() {
 
   function left() {
     let currentloc = Number(
-      document.getElementById("project1").style.marginLeft.split("px")[0]
+      document.getElementById("project2").style.marginLeft.split("px")[0]
     );
 
     if (currentloc === 0) return;
 
     currentloc += 400;
 
-    document.getElementById("project1").style.marginLeft = currentloc + "px";
+    document.getElementById("project2").style.marginLeft = currentloc + "px";
   }
 
 
   function right() {
     let currentloc = Number(
-      document.getElementById("project1").style.marginLeft.split("px")[0]
+      document.getElementById("project2").style.marginLeft.split("px")[0]
     );
 
     if (currentloc === -2100) return;
 
     currentloc -= 400;
 
-    document.getElementById("project1").style.marginLeft = currentloc + "px";
+    document.getElementById("project2").style.marginLeft = currentloc + "px";
   }
 
 
@@ -50,22 +50,12 @@ function Projects() {
         <div
           className="border border-light text-white p-3 d-flex gap-5 overflow-hidden"
           id="project1"
-          style={{ marginLeft: "-210" }}
+        
         >
-          <div className="border bg-gradient pro">
-            <div className="mb-2">
-              <img src={"/pro1.png"} width={"350px"} height={"300px"} />
-            </div>
-            <h4 className="fw-bold">Virtual Market Place - </h4>
-            <p className="fs-6 fw-medium">
-              a Market for Fresher To Start Earning And <br />
-              Showcase Their Work <br />
-              <span className="fs-5 fw-bold cont4">Made In MERN </span> <br />
-              More Info- <br />- A platform for freelancers and clients to
-              collaborate efficiently. - Integrated secure payment gateway,
-              escrow system, real-time chat, and feedback system.
-            </p>
-          </div>
+
+
+          <div className=" d-flex gap-5 rounded-3 " id="project2"   style={{ marginLeft: "-210" }}>
+
 
           <div className="border bg-gradient pro">
             <div className="mb-2">
@@ -141,6 +131,24 @@ function Projects() {
               escrow system, real-time chat, and feedback system.
             </p>
           </div>
+
+          <div className="border bg-gradient pro">
+            <div className="mb-2">
+              <img src={"/pro1.png"} width={"350px"} height={"300px"} />
+            </div>
+            <h4 className="fw-bold">Virtual Market Place - </h4>
+            <p className="fs-6 fw-medium">
+              a Market for Fresher To Start Earning And <br />
+              Showcase Their Work <br />
+              <span className="fs-5 fw-bold cont4">Made In MERN </span> <br />
+              More Info- <br />- A platform for freelancers and clients to
+              collaborate efficiently. - Integrated secure payment gateway,
+              escrow system, real-time chat, and feedback system.
+            </p>
+          </div>
+
+          </div>
+          
         </div>
 
         <button onClick={left} id="left">
